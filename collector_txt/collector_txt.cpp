723c64e7b9af1eb78221875fc8ded30226c4c694
kullkay <104859787+kullkay@users.noreply.github.com>
2023-01-24 20:54:32 +0300

    Add files via upload

---
+++ collector_txt/collector_txt.cpp
@@ -0,0 +1,25 @@
+﻿
+#include <fstream>
+
+#include <iostream>
+#include <string>
+#include <filesystem>
+
+using namespace std;
+const string path = "C:\\Users\\Andrew\\Desktop\\цели\\дневнк";
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    
+    
+
+    for (const auto& path_to_file : filesystem::directory_iterator(path))
+    {
+        ifstream file(path_to_file.path().string());
+        char s[255];
+        file.getline(s, 255, '\n');
+        cout << s <<endl;
+    }
+}
+
+
